81fb33d218465eb1495e4bc9690646f8180e54fe
Slawomir Trzaszczka <slawomir.trzaszczka@allegrogroup.com>
2017-04-20 16:41:23 +0200

    dist sensor added

--- robot.py
+++ robot.py
@@ -7,7 +7,7 @@ def hello():
  time.sleep(.5)
  stop()
 
-def sensor():
+def distance_sensor():
   distance_sensor_pin = analogPort
   while True:
     try:
@@ -17,9 +17,22 @@ def sensor():
     except IOError:
         print ("Error")
 
+def distance_sensor_test():
+  distance_sensor_pin = analogPort
+  while True:
+    try:
+        distance = us_dist(distance_sensor_pin)
+        if distance < 5:
+	  led_on(1)
+	else:
+	  lef_off(1)
+        time.sleep(.5)
+
+    except IOError:
+        print ("Error")
+ 
+
 print("start")
 enable_com_timeout(2000)
-while True:
- sensor()
- break
+distance_sensor_test()
 print("end")
