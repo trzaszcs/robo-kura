115d625bc229dd8627e6e44fc151f94c0b4307ca
Slawomir Trzaszczka <slawomir.trzaszczka@allegrogroup.com>
2017-04-20 15:51:38 +0200

    methods

--- robot.py
+++ robot.py
@@ -1,6 +1,24 @@
 from gopigo import *
+import time
+
+
+def hello():
+ forward()
+ time.sleep(.5)
+ stop()
+
+def sensor():
+  distance_sensor_pin = gopigo.analogPort
+  while True:
+    try:
+        print (gopigo.us_dist(distance_sensor_pin))
+        time.sleep(.5)
+
+    except IOError:
+        print ("Error")
 
 print("start")
-forward()
-stop()
+while True:
+ hello()
+ break
 print("end")
