88454df21469d406fc8d167afdb6a313b44b7a0f
Adrian Perek <the.adrian.perek@gmail.com>
2017-04-20 19:43:50 +0200

    camera library and camshot method added

--- robot.py
+++ robot.py
@@ -1,11 +1,19 @@
 from gopigo import *
 import time
-
+from picamera import PiCamera
 
 def hello():
  forward()
  time.sleep(.5)
  stop()
+
+def cumshot():
+ camera = PiCamera()
+ camera.resolution = (1024, 768)
+ camera.start_preview()
+ # Camera warm-up time
+ time.sleep(2)
+ camera.capture('foo.jpg')
 
 def distance_sensor():
   distance_sensor_pin = analogPort
@@ -23,6 +31,11 @@ def dziob():
  time.sleep(0.3)
  servo(25)
  time.sleep(1)
+ servo(80)
+ time.sleep(0.3)
+ servo(0)
+
+
 
 def distance_sensor_test():
   distance_sensor_pin = analogPort
@@ -39,7 +52,7 @@ def distance_sensor_test():
         print ("Error")
 
 def motion_sensor():
- motion_sensor_pin = digitalPort
+ motion_sensor_pin = 0
  pinMode(motion_sensor_pin,"INPUT")
  while True:
    try:
@@ -50,5 +63,6 @@ def motion_sensor():
 
 print("start")
 enable_com_timeout(2000)
-motion_sensor()
+dziob()
+cumshot()
 print("end")
